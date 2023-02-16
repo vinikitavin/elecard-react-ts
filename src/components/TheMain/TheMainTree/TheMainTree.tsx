0f4a6130402bf06e4eb932d847f89e4a2628ecf5
vinikitavin <vinikitavin@mail.ru>
2023-02-16 23:32:57 +0700

    finished tree list

--- src/components/TheMain/TheMainTree/TheMainTree.tsx
+++ src/components/TheMain/TheMainTree/TheMainTree.tsx
@@ -8,6 +8,7 @@ interface IMainTreeProps {
 
 export function TheMainTree({card}: IMainTreeProps): JSX.Element {
     const [isModal, setIsModal] = useState(false)
+    const [isOpenedList, setIsOpenedList] = useState(false)
 
     function openModal() {
         setIsModal(true)
@@ -17,33 +18,43 @@ export function TheMainTree({card}: IMainTreeProps): JSX.Element {
         setIsModal(false)
     }
 
+    function openList() {
+        const summary: HTMLElement | null = document.getElementById('summary')
+        setIsOpenedList(!isOpenedList)
+        summary!.click()
+    }
+
     return (
         <div className="main-tree">
-            <div className="main-tree__tree-padding main-tree__tree-summaries">
-                <div>
-                    <details>
-                        <summary className="main-tree__header">Карточка №{card.id}</summary>
-                        <ul className="main-tree__list">
-                            <li>
-                                Размер файла: { Math.round(Number(card.filesize) / 1024) } КБ
-                            </li>
-                            <li>
-                                Дата: {timeConverter(card.timestamp)}
-                            </li>
-                            <li>
-                                Категория: {firstCursiveLetter(card.category)}
-                            </li>
-                            <li>
-                                <p className="main-tree__img-head">Картинка:</p>
-                                <img alt="tree-img"
-                                     className="main-tree__img"
-                                     src={`http://contest.elecard.ru/frontend_data/${card.image}`}
-                                     onClick={openModal}
-                                />
-                            </li>
-                        </ul>
-                    </details>
-                </div>
+            <div className="main-tree__tree-summaries">
+                {isOpenedList ? (
+                        <img onClick={openList} alt="minus-icon" className="main-tree__minus" src="minus.svg"/>
+                    )
+                    : (
+                        <img onClick={openList} alt="plus-icon" className="main-tree__plus" src="plus.svg"/>
+                    )}
+                <details>
+                    <summary onClick={() => setIsOpenedList(!isOpenedList)} id="summary" className="main-tree__header">Карточка №{card.id}</summary>
+                    <ul className="main-tree__list">
+                        <li>
+                            Размер файла: {Math.round(Number(card.filesize) / 1024)} КБ
+                        </li>
+                        <li>
+                            Дата: {timeConverter(card.timestamp)}
+                        </li>
+                        <li>
+                            Категория: {firstCursiveLetter(card.category)}
+                        </li>
+                        <li>
+                            <p className="main-tree__img-head">Картинка:</p>
+                            <img alt="tree-img"
+                                 className="main-tree__img"
+                                 src={`http://contest.elecard.ru/frontend_data/${card.image}`}
+                                 onClick={openModal}
+                            />
+                        </li>
+                    </ul>
+                </details>
             </div>
             {isModal ? (
                 <div className="modal__mask">
